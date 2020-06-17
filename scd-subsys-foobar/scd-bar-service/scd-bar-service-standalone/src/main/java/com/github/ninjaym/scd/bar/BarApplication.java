da402e2a5fee17dd2c7dd4952dff66bf7a6b0cbe
ym <ninjay.ym@foxmail.com>
2020-06-17 13:39:36 +0800

    Check if loader balance work

--- scd-subsys-foobar/scd-bar-service/scd-bar-service-standalone/src/main/java/com/github/ninjaym/scd/bar/BarApplication.java
+++ scd-subsys-foobar/scd-bar-service/scd-bar-service-standalone/src/main/java/com/github/ninjaym/scd/bar/BarApplication.java
@@ -1,8 +1,8 @@
 package com.github.ninjaym.scd.bar;
 
 import com.github.ninjaym.scd.commons.service.ScdService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @ScdService
 public class BarApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(BarApplication.class, args);
     }
 
     @RestController
     public static class AppController {
+
+        @Value("${spring.cloud.client.hostname}:${spring.application.name}:${spring.application.instance_id:${server.port}}")
+        private String instanceId;
+
         @GetMapping("/hello")
         public String hello() {
-            return "hello world from bar-service";
+            return String.format("hello world from bar-service (%s)", instanceId);
         }
     }
 }
